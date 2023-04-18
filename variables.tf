5966cee7ec89272d71aaba5104f9a1c980396b93
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2023-04-18 07:36:50 +0200

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  type        = string
+  description = "location of the resource group"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Name of the storage account"
+}
+
+variable "count_value" {
+  type        = number
+  description = "number of storage accounts"
+}
